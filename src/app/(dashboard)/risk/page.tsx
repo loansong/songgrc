eafeba7e61bba0d2f9b3f0b7f51f6f86f068ec38
loansong <loansong@gmail.com>
2026-03-25 11:20:01 +0000

    Update page.tsx

--- src/app/(dashboard)/risk/page.tsx
+++ src/app/(dashboard)/risk/page.tsx
@@ -1,5 +1,5 @@
-"use client"
 import { cn } from "@/lib/utils";
+"use client"
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
 import { Button } from "@/components/ui/button"
 import { Badge } from "@/components/ui/badge"
